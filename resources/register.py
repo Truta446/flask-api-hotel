ca090d2c9ef80d9e50e2e5de807f28a4bff42ba2
Juan V Lopes <juan.versolato@outlook.com>
2020-02-09 11:15:22 -0400

    When register, send email to confirm user settings

--- resources/register.py
+++ resources/register.py
@@ -1,14 +1,16 @@
 from flask_restful import Resource, reqparse
 from models.user import UserModel
+import traceback
 
 
 class UserRegister(Resource):
     def post(self):
         attributes = reqparse.RequestParser()
-        attributes.add_argument(
-            'email', type=str, required=True, help="The field 'email' cannot be left blank.")
+        attributes.add_argument('email', type=str, required=True,
+                                help="The field 'email' cannot be left blank.")
         attributes.add_argument('password', type=str, required=True,
                                 help="The field 'password' cannot be left blank.")
+        attributes.add_argument('active', type=bool)
 
         data = attributes.parse_args()
 
@@ -16,6 +18,13 @@ class UserRegister(Resource):
             return {'message': f"The email '{data['email']}' already exists."}, 400
 
         user = UserModel(**data)
-        user_id = user.save_user()
+        user.active = False  # Security
 
-        return {'message': f'User {user_id} created successfully!'}, 201
+        try:
+            user_id = user.save_user()
+            user.send_confirmation_email()
+            return {'message': f'User {user_id} created successfully!'}, 201
+        except Exception as ex:
+            user.delete_user()
+            traceback.print_exc()
+            return {'message': f'An internal server error has occured: {ex}.'}, 500
