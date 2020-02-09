8fe26f0ca69a1c64aec503ee32bae2b8159ed63a
Juan V Lopes <juan.versolato@outlook.com>
2020-02-09 11:13:51 -0400

    User active validation

--- resources/login.py
+++ resources/login.py
@@ -8,17 +8,21 @@ class UserLogin(Resource):
     @classmethod
     def post(cls):
         attributes = reqparse.RequestParser()
-        attributes.add_argument(
-            'email', type=str, required=True, help="The field 'email' cannot be left blank.")
+        attributes.add_argument('email', type=str, required=True,
+                                help="The field 'email' cannot be left blank.")
         attributes.add_argument('password', type=str, required=True,
                                 help="The field 'password' cannot be left blank.")
+        attributes.add_argument('active', type=bool)
 
         data = attributes.parse_args()
 
         user = UserModel.find_by_email(data['email'])
 
         if user and safe_str_cmp(user.password, data['password']):
-            token = create_access_token(identity=user.user_id)
-            return {'message': 'Welcome!', 'access_token': token}, 200
+            if user.active:
+                token = create_access_token(identity=user.user_id)
+                return {'message': 'Welcome!', 'access_token': token}, 200
+
+            return {'message': 'User not confirmed.'}, 403
 
         return {'message': 'The email or password is incorrect'}, 401
